b5a6a44b8c9964278d1db4934dde02b91401484b
boblightning <satrianugraha4000@gmail.com>
2021-11-24 00:07:35 +0700

    DaftarMasuk

--- src/Component/Form.jsx
+++ src/Component/Form.jsx
@@ -10,7 +10,10 @@ class Form extends React.Component {
             username: "",
             email: "",
             password: "",
-            role: "User",
+            confirmpassword: "",
+            masukemail: "",
+            masukpassword: "",
+            role: "user",
             dataUser: []
         }
     }
@@ -29,26 +32,49 @@ class Form extends React.Component {
     }
 
     btDaftar = () => {
-        let { username, email, password } = this.state
-        axios.post(`http://localhost:2000/dataUser`, {
-            username, email, password, role: "User"
-        }).then((response) => {
-            this.getData()
-            this.setState({
-                username:"",
-                email:"",
-                password:"",
-                role:"User"
+        let { username, email, password, confirmpassword } = this.state;
+        if (password === confirmpassword) {
+            axios.post(`http://localhost:2000/dataUser`, {
+                username, email, password, role: "user"
+            }).then((response) => {
+                this.getData()
+                this.setState({
+                    username: "",
+                    email: "",
+                    password: "",
+                    confirmpassword: "",
+                    role: "user"
+                })
+                alert("Berhasil!")
+            }).catch((err) => {
+                console.log(err)
             })
-        }).catch((err)=>{
-            console.log(err)
-        })
+        } else {
+            alert("Pendaftaran Gagal")
+        }
+    }
+    btMasuk = () => {
+        let{dataUser, masukemail, masukpassword} = this.state;
+        let index = null;
+        for (let i = 0; i < dataUser.length; i++) {
+            if (dataUser[i].email === masukemail && dataUser[i].password === masukpassword) {
+                index = i
+            }
+        }
+        if (index != null) {
+            alert(`${dataUser[index].username} Masuk`)
+            this.setState({
+                masukemail: "",
+                masukpassword: "",
+            })
+        } else {
+            alert("Login Gagal")
+        }
     }
 
-
-    handleInput=(value,propState)=>{
-        console.log(value,propState)
-        this.setState({[propState]:value})
+    handleInput = (value, propState) => {
+        console.log(value, propState)
+        this.setState({ [propState]: value })
     }
 
     render() {
@@ -64,10 +90,10 @@ class Form extends React.Component {
                             <h3>Silahkan Masuk ke akun Anda</h3>
                             <p>Silahkan Masuk ke akun Anda untuk menyesuaikan pembayaran data pribadi Anda</p>
                             <p>Email</p>
-                            <input type="text" className="form-control" placeholder="Email" aria-label="Email" />
+                            <input type="text" className="form-control" placeholder="Email" aria-label="Email" onChange={(event) => this.handleInput(event.target.value, "masukemail")} />
                             <p style={{ marginRight: "auto" }}>Password</p>
-                            <input type="text" className="form-control" placeholder="Password" aria-label="Password" />
-                            <button type="submit" className="btn btn-primary" style={{ width: "640px", marginTop: "2vh" }}>Masuk</button>
+                            <input type="text" className="form-control" placeholder="Password" aria-label="Password" onChange={(event) => this.handleInput(event.target.value, "masukpassword")} />
+                            <button type="submit" className="btn btn-primary" style={{ marginTop: "2vh" }} onClick={this.btMasuk}>Masuk</button>
                         </div>
                         <div class="col">
                             <h3>Silahkan buat akun Anda</h3>
@@ -75,12 +101,12 @@ class Form extends React.Component {
                             <p>Username</p>
                             <input type="text" className="form-control" placeholder="username" aria-label="username" onChange={(event) => this.handleInput(event.target.value, "username")} />
                             <p>Email</p>
-                            <input type="text" className="form-control" placeholder="email" aria-label="email" onChange={(event) => this.handleInput(event.target.value, "email")}/>
+                            <input type="text" className="form-control" placeholder="email" aria-label="email" onChange={(event) => this.handleInput(event.target.value, "email")} />
                             <p>Password</p>
-                            <input type="text" className="form-control" placeholder="password" aria-label="password" onChange={(event) => this.handleInput(event.target.value, "password")}/>
+                            <input type="text" className="form-control" placeholder="password" aria-label="password" onChange={(event) => this.handleInput(event.target.value, "password")} />
                             <p>Confirmation Password</p>
-                            <input type="text" className="form-control" placeholder="Confirmation Pasword" aria-label="Confirmation Password" />
-                            <button type="submit" className="btn btn-primary" style={{ width: "640px", marginTop: "2vh" }} onClick={this.btDaftar}>Daftar</button>
+                            <input type="text" className="form-control" placeholder="Confirmation Pasword" aria-label="Confirmation Password" onChange={(event) => this.handleInput(event.target.value, "confirmpassword")} />
+                            <button type="submit" className="btn btn-primary" style={{ marginTop: "2vh" }} onClick={this.btDaftar}>Daftar</button>
                         </div>
                     </div>
                 </div>
